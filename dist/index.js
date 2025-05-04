40f8ad9b9b46a9b16004c71f1e2b62eab0c8b095
Gabegi <gabriel_pirastru@hotmail.fr>
2025-05-04 19:47:52 +0400

    tst

--- dist/index.js
+++ dist/index.js
@@ -4,4 +4,12 @@ let age = 20;
 if (age < 50)
     age += 10;
 console.log(age);
+let sales = 123456789;
+let course = "TypeScript";
+let is_published = false;
+let mytype;
+mytype = 1;
+mytype = "hello";
+let numbers = [];
+let user = [1, "Bob"];
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,15 @@ if (age < 50) age += 10;
 console.log(age);
 
 let sales: number = 123_456_789;
+
 let course: string = "TypeScript";
 let is_published: boolean = false;
+
+let mytype;
+
+mytype = 1;
+mytype = "hello";
+
+let numbers: number[] = [];
+
+let user: [number, string] = [1, "Bob"];
