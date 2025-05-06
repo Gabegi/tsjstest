90cd6bc58ef86498c3bc5bc81940d1d687f77414
Gabegi <gabriel_pirastru@hotmail.fr>
2025-05-06 06:02:32 +0200

    tst

--- src/index.ts
+++ src/index.ts
@@ -49,7 +49,39 @@ let mySize: Size = Size.large;
 console.log(mySize); // 2
 
 // (variable it takes): return type
-function calculateTax(income: number): number {
+function calculateTax(income: number, taxYear = 2022): number {
   if (income < 50000) return income * 1.2;
+  if (taxYear < 2022) return income * 1.3;
   else return 0; //undefined
 }
+
+calculateTax(50000, 2020); // unlike js ts doesnt allow more params than specified
+
+// objects => dynamic, can change over the course of the programme
+let employee: {
+  readonly id: number;
+  name?: string;
+  retire: (date: Date) => void;
+} = {
+  id: 1,
+  retire: (date: Date) => {
+    console.log(date);
+  },
+};
+
+// employee.id = 2; not allowed (readonly)
+
+// type alias = define a custom type
+type Employee = {
+  readonly id: number;
+  name: string;
+  retire: (date: Date) => void;
+};
+
+let employee2: Employee = {
+  id: 1,
+  name: "Bob",
+  retire: (date: Date) => {
+    console.log(date);
+  },
+};
